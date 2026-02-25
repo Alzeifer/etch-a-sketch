27bfc7fc5134a947560b9f013683fa977e256b44
Alzeifer <alzeifer123@gmail.com>
2026-02-25 13:58:53 -0500

    randomize colors

--- main.js
+++ main.js
@@ -35,7 +35,15 @@ function createGrid(size){
         container.append(box);
     
     box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "rgb(248, 59, 59)";
+        box.style.backgroundColor = randomColor();
+
+        function randomColor(){
+            const red =  Math.floor(Math.random() * 256);
+            const green = Math.floor(Math.random() * 256);
+            const blue = Math.floor(Math.random() * 256);
+
+            return `rgb(${red} ${green} ${blue})`;
+        }
     })
 }
 }
